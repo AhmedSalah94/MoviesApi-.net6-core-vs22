951911d502aacfa74a8f958116234b81e4be77fe
Ahmed_Salah <AhmedSalah7223@gmail.com>
2022-03-10 15:45:50 +0200

    upload project feom pc

---
+++ MoviesApi/Services/IGenresService.cs
@@ -0,0 +1,16 @@
+﻿using MoviesApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MoviesApi.Services
+{
+    public interface IGenresService
+    {
+        Task<IEnumerable<Genre>> GetAll();
+        Task<Genre> GetById(byte id);
+        Task<Genre> Add(Genre genre);
+        Genre Update(Genre genre);
+        Genre Delete(Genre genre);
+        Task<bool> IsvalidGenre(byte id);
+    }
+}
---
+++ MoviesApi/Services/IMoviesService.cs
@@ -0,0 +1,15 @@
+﻿using MoviesApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MoviesApi.Services
+{
+    public interface IMoviesService
+    {
+        Task<IEnumerable<Movie>> GetAll(byte genreId = 0);  //optional param
+        Task<Movie> GetById(int id);
+        Task<Movie> Add(Movie movie);
+        Movie Update(Movie movie);
+        Movie Delete(Movie movie);
+    }
+}
